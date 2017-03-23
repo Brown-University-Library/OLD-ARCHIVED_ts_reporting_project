9d3359d99d8b23acfeaff9448c60e8dc39b4c96b
birkin <birkin.diana@gmail.com>
2017-03-23 11:06:41 -0400

    cleans up admin.

--- tech_services_reports/admin.py
+++ tech_services_reports/admin.py
@@ -2,12 +2,8 @@
 
 from __future__ import unicode_literals
 
+from .models import Accession, CatEdit, Harvest, SummaryAccession, SummaryCatEdit
 from django.contrib import admin
-
-
-
-from .models import SummaryAccession, CatEdit, Accession, Harvest, SummaryCatEdit
-# from django.contrib import admin
 
 
 class AccessionAdmin(admin.ModelAdmin):
@@ -15,10 +11,6 @@ class AccessionAdmin(admin.ModelAdmin):
     search_fields = ('created', 'format', 'location', 'acquisition_method')
     list_filter = ('acquisition_method', 'format', 'location')
 
-#class CatalogingAdmin(admin.ModelAdmin):
-#    list_display = ('cat_date', 'cat_type', 'level')
-#    search_fields = ('cat_date', 'cat_type', 'level')
-#    list_filter = ('cat_type', 'level', 'mat_type')
 
 class CatEditAdmin(admin.ModelAdmin):
     list_display = ('edit_date', 'editor', 'type', 'mat_type', 'source', 'bib')
@@ -26,10 +18,9 @@ class CatEditAdmin(admin.ModelAdmin):
     search_fields = ('editor', 'bib', 'type', 'source')
     #list_filter = ('cat_type', 'level', 'mat_type')
 
+
 admin.site.register(Harvest)
 admin.site.register(Accession, AccessionAdmin)
-#admin.site.register(Cataloging, CatalogingAdmin)
 admin.site.register(SummaryAccession)
 admin.site.register(CatEdit, CatEditAdmin)
 admin.site.register(SummaryCatEdit)
-#admin.site.register(BibItem, BibItemAdmin)
