f37206b363c4ba9c2c45028a1981ea0736a8264b
birkin <birkin.diana@gmail.com>
2017-03-24 10:05:33 -0400

    adds setting.

--- tech_services_reports/settings_app.py
+++ tech_services_reports/settings_app.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 import os
 
 
-FOO = 'BAR'
+#####################################################
+### for tech_services_reports/management/commands ###
+#####################################################
 
-# FOO2 = os.environ['FOO2']
+## ts_reports_loader.py
+LOCATION_FORMAT_URL = 'http://library.brown.edu/services/location_format/v1/?data=dump'
