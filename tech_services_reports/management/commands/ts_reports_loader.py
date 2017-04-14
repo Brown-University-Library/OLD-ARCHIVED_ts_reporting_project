f333b6e87d43c61a69f93237988b5920b661a7e7
birkin <birkin.diana@gmail.com>
2017-04-14 12:46:39 -0400

    starts breakout of code into libs.

--- tech_services_reports/management/commands/ts_reports_loader.py
+++ tech_services_reports/management/commands/ts_reports_loader.py
@@ -16,6 +16,9 @@ from django.core.cache import cache
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from tech_services_reports import settings_app
+from tech_services_reports.models import Accession, CatEdit, Harvest
+from tech_services_reports.utility_code import CatStat
+from tech_services_reports.utility_code import convert_date
 
 
 log = logging.getLogger( "processing" )
@@ -117,13 +120,9 @@ class Command(BaseCommand):
         return convert_date(d)
 
     def summary(self, marc_file):
-        """Harvests data points from exported MARC fields.
-        Date counts will include the date of a given harvest."""
+        """ Harvests data points from exported MARC fields.
+            Date counts will include the date of a given harvest. """
         log.debug( 'starting summary()' )
-        from tech_services_reports.utility_code import convert_date
-        from tech_services_reports.utility_code import CatStat
-        from tech_services_reports.models import Accession, CatEdit, Harvest
-        from datetime import date
         log.info( 'reading MARC file, ```{}```'.format(marc_file) )
         #Dicts to store counts
         cataloging_edit_count = {}
@@ -134,88 +133,25 @@ class Command(BaseCommand):
         #Add logic to skip counted items.
         log.info( 'retrieving existing items stored in Accessions database' )
         existing_items = self.counted_items()
+
+        #======================================================================
+        # parse marc file
+        #======================================================================
+
         #Loop through marc records.
         counter = 0
 
-        with open( marc_file, 'rb' ) as fh:
-            reader = pymarc.MARCReader( fh, to_unicode=True, force_utf8=True, utf8_handling='ignore' )
-            start = datetime.datetime.now()
-            count = 0
-            for record in reader:
-                # log.debug( 'record, ```{}```'.format(record) )
-                if counter > 0 and counter % 10000 == 0:
-                    # print>>sys.stderr, '`{}` records processed'.format(counter)
-                    log.info( '`{}` records processed'.format(counter) )
-                counter += 1
-                try:
-                    bib_number = record['907']['a'][1:]
-                    log.debug( 'bib_number, `{}`'.format(bib_number) )
-                except TypeError:
-                    log.debug( 'no bib_number' )
-                    continue
-                bib_level = record['998']['c']
-                bib_created = self.get_bib_created(record)
-                #==================================================================
-                # Count cat edits
-                #==================================================================
-                cat_date = convert_date(record['998']['b'])
-                cat_stat = CatStat(record)
-                #Count cataloging edits
-                #Store needed fields.
-                marc_995 = record.get_fields('995')
-                mat_type = cat_stat.mat_type()
-                source = cat_stat.cat_type()
-                #Batch edit notes stored here.
-                marc_910 = record.get_fields('910')
-                #Count the batch load info
-                this_batch_edit = self.count_batch_edits(
-                               bib_number,
-                               bib_created,
-                               mat_type,
-                               marc_910,
-                               cataloging_edit_count,
-                               source)
-                cataloging_edit_count.update(this_batch_edit)
+        data = marc_parser.parse_marc_file( marc_file )
+        log.debug( 'data, ```{}```'.format(data) )
 
-                #Count individual edits added by staff.
-                this_cat_edit = self.count_cataloging_edits(bib_number,
-                                                            mat_type,
-                                                            marc_995,
-                                                            cataloging_edit_count,
-                                                            source)
-                cataloging_edit_count.update(this_cat_edit)
-                #==================================================================
-                # Count accessions based off item fields.
-                #==================================================================
-                items = record.get_fields('945')
-                #Count the volumes
-                #This will be dict with a named tuple as a key.
-                this_count = self.count_volumes(items,
-                                                cat_date,
-                                                mat_type,
-                                                existing_items)
-                #We won't be counting everything - skipping some old items.
-                if this_count is None:
-                    continue
-                #Pull the volume and title count from the accessions key.
-                this_vol = this_count['volumes']
-                this_title = this_count['titles']
-
-                #Add the title count
-                for k, title in this_title.items():
-                    title_count[k] = title_count.get(k, 0) + title
-
-                #Add the volume count
-                #Iterate through item counts and update
-                for k, vol in this_vol.items():
-                    volume_count[k] = volume_count.get(k, 0) + vol
         log.debug( 'volume_count dct, ```{}```'.format( pprint.pformat(volume_count) ) )
 
         #Finish looping through MARC records
 
         #======================================================================
-        # Put the individual counts together into a db fixture.
+        # put individual counts together into  db fixture
         #======================================================================
+
         db_fixture = []
         # print>>sys.stderr, "Creating accessions stats fixture."
         log.info( 'creating accessions stats fixture' )
