e54af70e9bd72b8d8b3c9f8c93d532c95d938a51
birkin <birkin.diana@gmail.com>
2017-04-14 12:13:21 -0400

    updates env-settings reference.

--- manage.py
+++ manage.py
@@ -5,14 +5,14 @@ import dotenv
 if __name__ == "__main__":
 
     PROJECT_DIR = os.path.dirname( os.path.abspath(__file__) )
-    ENV_PATH = os.path.abspath( '{}/../ts_reporting_env_settings.env'.format(PROJECT_DIR) )
+    ENV_PATH = os.path.abspath( '{}/../local_settings.env'.format(PROJECT_DIR) )
     # print( 'ENV_PATH, ```{}```'.format(ENV_PATH) )
 
     # print( 'environ initially, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
     dotenv.read_dotenv( ENV_PATH )
     # print( 'environ now, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault( "DJANGO_SETTINGS_MODULE", "config.settings" )
 
     from django.core.management import execute_from_command_line
 
