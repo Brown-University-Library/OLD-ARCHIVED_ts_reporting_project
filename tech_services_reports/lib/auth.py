0e582508469408163a965336f47a2b23ff32e903
birkin <birkin.diana@gmail.com>
2017-05-17 11:15:32 -0400

    updates vars for sense.

--- tech_services_reports/lib/auth.py
+++ tech_services_reports/lib/auth.py
@@ -16,8 +16,8 @@ def bul_login(func):
     log.debug( 'starting bul_login() decorator' )
     def decorator(request, *args, **kwargs):
         hlpr = LoginDecoratorHelper()
-        shib_dict = hlpr.prep_shib_dct( request )
-        user_obj = hlpr.manage_usr_obj( request, shib_dict )
+        cleaned_meta_dct = hlpr.prep_shib_dct( request.META )
+        user_obj = hlpr.manage_usr_obj( request, cleaned_meta_dct )
         if not user_obj:
             return HttpResponseForbidden( '403 / Forbidden' )
         return func(request, *args, **kwargs)
@@ -27,58 +27,58 @@ def bul_login(func):
 class LoginDecoratorHelper(object):
     """ Handles login decorator code. """
 
-    def prep_shib_dct( self, request ):
+    def prep_shib_dct( self, request_meta_dct ):
         """ Returns dct from shib-info.
             Called by bul_login() """
         log.debug( 'starting prep_shib_dct()' )
-        new_dct = copy.copy( request.META )
+        new_dct = copy.copy( request_meta_dct )
         log.debug( 'new_dct, ```{}```'.format(pprint.pformat(new_dct)) )
-        for (key, val) in request.META.items():  # get rid of mod_wsgi dictionary items not serializable
+        for (key, val) in request_meta_dct.items():  # get rid of some dictionary items not serializable
             if 'passenger' in key:
                 new_dct.pop( key )
             elif 'wsgi.' in key:
                 new_dct.pop( key )
         return new_dct
 
-    def manage_usr_obj( self, request, data ):
+    def manage_usr_obj( self, request, meta_dct ):
         """ Pull information for the Shib request and get/create and login Django User object.
             Called by bul_login() """
-        ( username, netid ) = self.set_basics( request.get_host(), data )
+        ( username, netid ) = self.set_basics( request.get_host(), meta_dct )
         if not username or not netid:
             return
-        u = self.update_userobj( username, netid )
+        usr = self.update_userobj( username, netid )
         backend = get_backends()[0]
-        u.backend = '{module}.{classname}'.format( module=backend.__module__, classname=backend.__class__.__name__ )
-        login( request, u )  #Brute force login, see - http://djangosnippets.org/snippets/1552/
+        usr.backend = '{module}.{classname}'.format( module=backend.__module__, classname=backend.__class__.__name__ )
+        login( request, usr )  #Brute force login, see - http://djangosnippets.org/snippets/1552/
         log.debug( 'login complete' )
-        return u
+        return usr
 
-    def set_basics( self, host, request_dct ):
+    def set_basics( self, host, meta_dct ):
         """ Grabs username and netid.
             Called by manage_usr_obj() """
         if host == '127.0.0.1':
             username = settings_app.TEST_USERNAME
             netid = settings_app.TEST_NETID
         else:
-            username = request_dct.get('Shibboleth-eppn', None)
-            netid = request_dct.get('Shibboleth-brownNetId', None)
+            username = meta_dct.get('Shibboleth-eppn', None)
+            netid = meta_dct.get('Shibboleth-brownNetId', None)
         log.debug( 'username, `{usr}`; netid, `{net}`'.format( usr=username, net=netid ) )
         return ( username, netid )
 
     def update_userobj( self, username, netid ):
         """ Grabs user object, updates and saves it.
             Called by manage_usr_obj() """
-        u, created = User.objects.get_or_create( username=username.replace('@brown.edu', '').strip() )
-        if netid in settings_app.SUPER_USERS: u.is_superuser = True
-        if netid in settings_app.STAFF_USERS: u.is_staff = True
+        usr, created = User.objects.get_or_create( username=username.replace('@brown.edu', '').strip() )
+        if netid in settings_app.SUPER_USERS: usr.is_superuser = True
+        if netid in settings_app.STAFF_USERS: usr.is_staff = True
         if created:
-            u.first_name = data.get('Shibboleth-givenName', '')
-            u.last_name = data.get('Shibboleth-sn', '')
-            u.email = data.get('Shibboleth-mail', None)
-            u.set_unusable_password()
-        u.save()
+            usr.first_name = data.get('Shibboleth-givenName', '')
+            usr.last_name = data.get('Shibboleth-sn', '')
+            usr.email = data.get('Shibboleth-mail', None)
+            usr.set_unusable_password()
+        usr.save()
         log.debug( 'user updated and saved.' )
-        return u
+        return usr
 
     ## end class LoginDecoratorHelper()
 
