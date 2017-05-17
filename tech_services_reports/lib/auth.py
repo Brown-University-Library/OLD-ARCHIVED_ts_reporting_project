56b0c693ecc5439f47e1c0149abcf45c67cb685e
birkin <birkin.diana@gmail.com>
2017-05-17 11:08:57 -0400

    refactors auth decorator.

--- tech_services_reports/lib/auth.py
+++ tech_services_reports/lib/auth.py
@@ -15,67 +15,133 @@ def bul_login(func):
         Called by views.py decorators. """
     log.debug( 'starting bul_login() decorator' )
     def decorator(request, *args, **kwargs):
-        shib_dict = prep_shib_dct( request )
-        user_obj = pull_shib_info( request, shib_dict )
+        hlpr = LoginDecoratorHelper()
+        shib_dict = hlpr.prep_shib_dct( request )
+        user_obj = hlpr.manage_usr_obj( request, shib_dict )
         if not user_obj:
             return HttpResponseForbidden( '403 / Forbidden' )
         return func(request, *args, **kwargs)
     return decorator
 
 
-def prep_shib_dct( request ):
-    """ Returns dct from shib-info.
-        Called by bul_login() """
-    log.debug( 'starting prep_shib_dct()' )
-    new_dct = copy.copy( request.META )
-    log.debug( 'new_dct, ```{}```'.format(pprint.pformat(new_dct)) )
-    for (key, val) in request.META.items():  # get rid of mod_wsgi dictionary items not serializable
-        if 'passenger' in key:
-            new_dct.pop( key )
-        elif 'wsgi.' in key:
-            new_dct.pop( key )
-    return new_dct
+class LoginDecoratorHelper(object):
+    """ Handles login decorator code. """
+
+    def prep_shib_dct( self, request ):
+        """ Returns dct from shib-info.
+            Called by bul_login() """
+        log.debug( 'starting prep_shib_dct()' )
+        new_dct = copy.copy( request.META )
+        log.debug( 'new_dct, ```{}```'.format(pprint.pformat(new_dct)) )
+        for (key, val) in request.META.items():  # get rid of mod_wsgi dictionary items not serializable
+            if 'passenger' in key:
+                new_dct.pop( key )
+            elif 'wsgi.' in key:
+                new_dct.pop( key )
+        return new_dct
+
+    def manage_usr_obj( self, request, data ):
+        """ Pull information for the Shib request and get/create and login Django User object.
+            Called by bul_login() """
+        ( username, netid ) = self.set_basics( request.get_host(), data )
+        if not username or not netid:
+            return
+        u = self.update_userobj( username, netid )
+        backend = get_backends()[0]
+        u.backend = '{module}.{classname}'.format( module=backend.__module__, classname=backend.__class__.__name__ )
+        login( request, u )  #Brute force login, see - http://djangosnippets.org/snippets/1552/
+        log.debug( 'login complete' )
+        return u
+
+    def set_basics( self, host, request_dct ):
+        """ Grabs username and netid.
+            Called by manage_usr_obj() """
+        if host == '127.0.0.1':
+            username = settings_app.TEST_USERNAME
+            netid = settings_app.TEST_NETID
+        else:
+            username = request_dct.get('Shibboleth-eppn', None)
+            netid = request_dct.get('Shibboleth-brownNetId', None)
+        log.debug( 'username, `{usr}`; netid, `{net}`'.format( usr=username, net=netid ) )
+        return ( username, netid )
+
+    def update_userobj( self, username, netid ):
+        """ Grabs user object, updates and saves it.
+            Called by manage_usr_obj() """
+        u, created = User.objects.get_or_create( username=username.replace('@brown.edu', '').strip() )
+        if netid in settings_app.SUPER_USERS: u.is_superuser = True
+        if netid in settings_app.STAFF_USERS: u.is_staff = True
+        if created:
+            u.first_name = data.get('Shibboleth-givenName', '')
+            u.last_name = data.get('Shibboleth-sn', '')
+            u.email = data.get('Shibboleth-mail', None)
+            u.set_unusable_password()
+        u.save()
+        log.debug( 'user updated and saved.' )
+        return u
+
+    ## end class LoginDecoratorHelper()
 
 
-def pull_shib_info( request, data ):
-    """ Pull information for the Shib request and get/create and login Django User object.
-        Called by bul_login() """
-    ( username, netid ) = set_basics( request.get_host(), data )
-    if not username or not netid:
-        return
-    u = update_userobj( username, netid )
-    backend = get_backends()[0]
-    u.backend = '{module}.{classname}'.format( module=backend.__module__, classname=backend.__class__.__name__ )
-    login( request, u )  #Brute force login, see - http://djangosnippets.org/snippets/1552/
-    log.debug( 'login complete' )
-    return u
 
-def set_basics( host, request_dct ):
-    """ Grabs username and netid.
-        Called by pull_shib_info() """
-    if host == '127.0.0.1':
-        username = settings_app.TEST_USERNAME
-        netid = settings_app.TEST_NETID
-    else:
-        username = request_dct.get('Shibboleth-eppn', None)
-        netid = request_dct.get('Shibboleth-brownNetId', None)
-    log.debug( 'username, `{usr}`; netid, `{net}`'.format( usr=username, net=netid ) )
-    return ( username, netid )
+# def prep_shib_dct( request ):
+#     """ Returns dct from shib-info.
+#         Called by bul_login() """
+#     log.debug( 'starting prep_shib_dct()' )
+#     new_dct = copy.copy( request.META )
+#     log.debug( 'new_dct, ```{}```'.format(pprint.pformat(new_dct)) )
+#     for (key, val) in request.META.items():  # get rid of mod_wsgi dictionary items not serializable
+#         if 'passenger' in key:
+#             new_dct.pop( key )
+#         elif 'wsgi.' in key:
+#             new_dct.pop( key )
+#     return new_dct
 
-def update_userobj( username, netid ):
-    """ Grabs user object, updates and saves it.
-        Called by pull_shib_info() """
-    u, created = User.objects.get_or_create( username=username.replace('@brown.edu', '').strip() )
-    if netid in settings_app.SUPER_USERS: u.is_superuser = True
-    if netid in settings_app.STAFF_USERS: u.is_staff = True
-    if created:
-        u.first_name = data.get('Shibboleth-givenName', '')
-        u.last_name = data.get('Shibboleth-sn', '')
-        u.email = data.get('Shibboleth-mail', None)
-        u.set_unusable_password()
-    u.save()
-    log.debug( 'user updated and saved.' )
-    return u
+# def manage_usr_obj( request, data ):
+#     """ Pull information for the Shib request and get/create and login Django User object.
+#         Called by bul_login() """
+#     ( username, netid ) = set_basics( request.get_host(), data )
+#     if not username or not netid:
+#         return
+#     u = update_userobj( username, netid )
+#     backend = get_backends()[0]
+#     u.backend = '{module}.{classname}'.format( module=backend.__module__, classname=backend.__class__.__name__ )
+#     login( request, u )  #Brute force login, see - http://djangosnippets.org/snippets/1552/
+#     log.debug( 'login complete' )
+#     return u
+
+# def set_basics( host, request_dct ):
+#     """ Grabs username and netid.
+#         Called by manage_usr_obj() """
+#     if host == '127.0.0.1':
+#         username = settings_app.TEST_USERNAME
+#         netid = settings_app.TEST_NETID
+#     else:
+#         username = request_dct.get('Shibboleth-eppn', None)
+#         netid = request_dct.get('Shibboleth-brownNetId', None)
+#     log.debug( 'username, `{usr}`; netid, `{net}`'.format( usr=username, net=netid ) )
+#     return ( username, netid )
+
+# def update_userobj( username, netid ):
+#     """ Grabs user object, updates and saves it.
+#         Called by manage_usr_obj() """
+#     u, created = User.objects.get_or_create( username=username.replace('@brown.edu', '').strip() )
+#     if netid in settings_app.SUPER_USERS: u.is_superuser = True
+#     if netid in settings_app.STAFF_USERS: u.is_staff = True
+#     if created:
+#         u.first_name = data.get('Shibboleth-givenName', '')
+#         u.last_name = data.get('Shibboleth-sn', '')
+#         u.email = data.get('Shibboleth-mail', None)
+#         u.set_unusable_password()
+#     u.save()
+#     log.debug( 'user updated and saved.' )
+#     return u
+
+
+
+######################################
+
+
 
 # def pull_shib_info( request, data ):
 #     """ Pull information for the Shib request and get/create and login Django User object.
