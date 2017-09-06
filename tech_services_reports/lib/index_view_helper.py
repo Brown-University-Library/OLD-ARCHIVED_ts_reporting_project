2ecc2028a6cf1d623378eb661506556242a5cdfe
birkin <birkin.diana@gmail.com>
2017-09-06 13:56:01 -0400

    updates links.

--- tech_services_reports/lib/index_view_helper.py
+++ tech_services_reports/lib/index_view_helper.py
@@ -36,7 +36,8 @@ class DateMaker(object):
             acc_months = Accession.objects.dates('created', 'month', order='DESC')  # grabs list of accession date-objects, one-per-month
             cache.set( 'acc_months_cached', acc_months, 60*60*24 )  # 1 day
         for date_obj in acc_months:
-            link = '{sch}://{hst}{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year, mo=date_obj.month )
+            # link = '{sch}://{hst}{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year, mo=date_obj.month )
+            link = '{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year, mo=date_obj.month )
             acc_month_lst.append( { 'month': date_obj.month, 'month_name': calendar.month_name[date_obj.month], 'year': date_obj.year, 'link': link } )
         # log.debug( 'type(acc_months), `{typ}`; acc_months, ```{val}```'.format( typ=type(acc_months), val=acc_months) )
         log.debug( 'acc_month_lst, ```{}```'.format(pprint.pformat(acc_month_lst)) )
@@ -51,7 +52,8 @@ class DateMaker(object):
             cat_months = CatEdit.objects.dates('edit_date', 'month', order='DESC')  # grabs list of cataloging date-objects, one-per-month
             cache.set( 'cat_months_cached', cat_months, 60*60*24 )  # 1 day
         for date_obj in cat_months:
-            link = '{sch}://{hst}{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year, mo=date_obj.month )
+            # link = '{sch}://{hst}{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year, mo=date_obj.month )
+            link = '{url}{yr}/{mo}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year, mo=date_obj.month )
             cat_month_lst.append( { 'month': date_obj.month, 'month_name': calendar.month_name[date_obj.month], 'year': date_obj.year, 'link': link } )
         log.debug( 'cat_month_lst, ```{}```'.format(pprint.pformat(cat_month_lst)) )
         return cat_month_lst
@@ -65,7 +67,8 @@ class DateMaker(object):
             acc_years = Accession.objects.dates('created', 'year', order='DESC')  # grabs list of accession date-objects, one-per-year
             cache.set( 'acc_years_cached', acc_years, 60*60*24 )
         for date_obj in acc_years:
-            link = '{sch}://{hst}{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year )
+            # link = '{sch}://{hst}{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year )
+            link = '{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('accessions'), yr=date_obj.year )
             acc_years_lst.append( {'year': date_obj.year, 'link': link} )
         log.debug( 'acc_years_lst, ```{}```'.format(pprint.pformat(acc_years_lst)) )
         return acc_years_lst
@@ -79,7 +82,8 @@ class DateMaker(object):
             cat_years = CatEdit.objects.dates('edit_date', 'year', order='DESC')  # grabs list of cataloging date-objects, one-per-year
             cache.set( 'cat_years_cached', cat_years, 60*60*24 )
         for date_obj in cat_years:
-            link = '{sch}://{hst}{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year )
+            # link = '{sch}://{hst}{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year )
+            link = '{url}{yr}/'.format( sch=scheme, hst=host, url=reverse('cataloging'), yr=date_obj.year )
             cat_years_lst.append( {'year': date_obj.year, 'link': link} )
         log.debug( 'cat_years_lst, ```{}```'.format(pprint.pformat(cat_years_lst)) )
         return cat_years_lst
